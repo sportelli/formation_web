048687d64688c1ebb3ad42269cea629975f29938
felix <felix@sportelli.capital>
2017-11-10 16:13:54 +0100

    exercice 28

---
+++ js/exercice28.js
@@ -0,0 +1,17 @@
+var i=0;
+
+// Méthode à appeler 
+// sur clic utilisateur 
+function disBonjour()
+{
+	console.log("bonjour madame ...");
+}
+
+// Méthode à appeler 
+// si utilisateur sur image
+function passageImage()
+{
+	console.log("Passage n°" + ++i + " sur l'image");
+}
+
+ 
