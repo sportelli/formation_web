ec807cc1d877ce302be3825ff018b42ae45c2dff
felix <felix@sportelli.capital>
2017-11-27 10:53:11 +0100

    exercice 8 corrigé

--- html5/js/fonctions.js
+++ html5/js/fonctions.js
@@ -1,3 +1,6 @@
+var EDIT_IMG = "images/edit.png";
+var DELETE_IMG = "images/delete.png";
+
 function ajoutUtilisateurDB(user)
 {
 	var utilisateurs = [];
@@ -5,8 +8,26 @@ function ajoutUtilisateurDB(user)
 	{
 		utilisateurs = JSON.parse(localStorage["users"]);
 	}
+
+	// On définit un id à user qui vaut la taille (actuelle) de users
+	// +1 pour commencer à 1
+	user.id = utilisateurs.length +1 ;
+
 	utilisateurs.push(user);
 	localStorage["users"] = JSON.stringify(utilisateurs);
+}
+
+function deleteUserDB(idUser)
+{
+	if ( localStorage["users"] != null ) 
+	{
+		var utilisateurs = JSON.parse(localStorage["users"]);
+		for (var i= 0 ; i < utilisateurs.length ; i++)
+			if (utilisateurs[i].id === idUser)
+				delete utilisateurs[i];
+
+		localStorage["users"] = JSON.stringify(utilisateurs.filter(function(i){ return i != null; }));
+	}
 }
 
 function ajouteUtilisateursClic()
@@ -34,9 +55,39 @@ function dessineTableau()
 		var tab = document.querySelector("#bodytab");
 		var str= "";
 		for (var i = 0; i < utilisateurs.length; i++)
-		{
-			str += "<tr><td>" + utilisateurs[i].nom + "</td><td>" + utilisateurs[i].prenom + "</td></tr>";
+		{ 
+			if (utilisateurs[i] != null)
+			{
+			str += "<tr><td>" 
+						+ utilisateurs[i].nom + "</td><td>" 
+						+ utilisateurs[i].prenom + "</td>" 
+						+ "<td><img src='"+EDIT_IMG+"' alt='edit button' onClick='editUser("+utilisateurs[i].id+")'></td>"
+						+ "<td><img src='"+DELETE_IMG+"' alt='delete button' onClick='deleteUser("+utilisateurs[i].id+")'></td>"						
+						+"</tr>";
+			}
 		}
 		tab.innerHTML = str;
 	}
+}
+
+/**
+* Cette fonction va lancer l'edition d'un utilisateur sur le champs de saisie
+*
+* @arg id utilisateur 
+*/
+function editUser(idUser)
+{
+	console.log("edition utilisateur " + idUser);
+}
+
+/**
+* Cette fonction va lancer la suppression d'un utilisateur 
+*
+* @arg id utilisateur 
+*/
+function deleteUser(idUser)
+{
+	console.log("suppression utilisateur " + idUser);
+	deleteUserDB(idUser);
+	dessineTableau();	
 }
